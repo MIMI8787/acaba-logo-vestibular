8f1b436aa8369142243fb99ea933363dfdb7a9d9
MIMI8787 <140844491+MIMI8787@users.noreply.github.com>
2024-09-17 08:13:24 -0300

    d

---
+++ perguntas.js
@@ -0,0 +1,99 @@
+criaCartao(
+    ' Biologia',
+    'O que é a fotossíntese? ',
+    'A fotossíntese é o processo pelo qual as plantas, algas e algumas bactérias transformam energia solar, água e dióxido de carbono em glicose e oxigênio.')
+
+criaCartao(
+    'Química',
+    'Qual é a diferença entre uma ligação iônica e uma ligação covalente? ',
+    'Na ligação iônica, há transferência de elétrons entre átomos, formando íons. Na ligação covalente, os átomos compartilham pares de elétrons.')
+
+criaCartao(
+    'Física',
+    'O que diz a Primeira Lei de Newton?',
+    ' A Primeira Lei de Newton afirma que um corpo permanece em repouso ou em movimento retilíneo uniforme, a menos que uma força externa atue sobre ele.')
+
+criaCartao(
+    'Matemática',
+    'O que é uma função do segundo grau?',
+    'Uma função do segundo grau é uma função polinomial de grau 2')
+
+criaCartao(
+    ' História',
+    'Qual foi a principal causa da Primeira Guerra Mundial? ',
+    ' A principal causa foi o assassinato do arquiduque Francisco Ferdinando, herdeiro do trono austro-húngaro, em 1914, somado às tensões nacionalistas, imperialistas e alianças entre países.')
+
+criaCartao(
+    'Geografia',
+    ' O que é a globalização?',
+    'Globalização é o processo de integração econômica, cultural, social e política entre os países, facilitado por avanços em transporte e comunicação.')
+
+criaCartao(
+    ' Literatura',
+    'Quem é o autor de "Dom Casmurro"?',
+    'O autor de "Dom Casmurro" é Machado de Assis.')
+
+criaCartao(
+    'Filosofia',
+    'Qual é o principal conceito do "mito da caverna" de Platão? ',
+    'O "mito da caverna" representa a ilusão dos sentidos e a ignorância, sugerindo que o conhecimento verdadeiro só é alcançado fora do "mundo das sombras", ou seja, pela razão e filosofia.')
+
+criaCartao(
+    'Sociologia',
+    'O que é o conceito de "fato social" de Émile Durkheim? ',
+    'Fato social é qualquer maneira de agir, pensar e sentir que exerce uma influência externa sobre o indivíduo, sendo comum ao coletivo da sociedade.')
+
+criaCartao(
+    ' Biologia',
+    ' Qual é a função do sistema imunológico?',
+    'O sistema imunológico protege o organismo contra agentes patogênicos, como vírus, bactérias e parasitas, reconhecendo e eliminando essas ameaças.')
+    
+criaCartao(
+    ' Química',
+    'O que é o pH?',
+    'O pH é uma escala que mede a acidez ou basicidade de uma solução, variando de 0 a 14, onde 7 é neutro, abaixo de 7 é ácido e acima de 7 é básico.')
+    
+ criaCartao(
+    'Física',
+    'O que é a velocidade escalar média? ',
+    'A velocidade escalar média é a razão entre a distância total percorrida por um corpo e o tempo total gasto nesse percurso.')
+    
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta')
+    
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta')
+    
+ criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta')
+    
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta')
+    
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta')
+    
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta')
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta')
+        
+ criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta')
